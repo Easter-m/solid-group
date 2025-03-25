63829cc5db0b8d8932b67b59d6e7e088bf2ef5ae
ruthmuthama <ruth.muthama@strathmore.edu>
2025-03-25 19:25:30 +0300

    Database connection done by member D💋

---
+++ jdbc.pstgresql.connection/src/mypackage/postgresql/JDBCPostgresql.java
@@ -0,0 +1,42 @@
+package mypackage.postgresql;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class JDBCPostgresql {
+    private final String url="jdbc:postgresql://localhost:5432/ University management system";
+    private final String user="postgres";
+    private final String password="0745563712";
+
+    private void connect(){
+        System.out.println("Connecting to database...");
+        try(Connection conn= DriverManager.getConnection(url,user,password);) {
+            if(conn!=null){
+                System.out.println("Connected to server successfully");
+            }else {
+                System.out.println("Failed to connect to server");
+            }
+            Connection connection = null;
+            Statement statement= connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT VERSION()");
+            if(resultSet.next()){
+                System.out.println("Select ;"+resultSet.getString(1));
+            }
+
+
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        JDBCPostgresqlConnect sqlConnect = new JDBCPostgresqlConnect();
+        sqlConnect. connect();
+
+    }
+}
